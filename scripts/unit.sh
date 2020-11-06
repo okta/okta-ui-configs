6fef8fa65e228f910a36695052f5903479819b9b
jmelberg <jordan.melberg@okta.com>
2020-11-05 17:17:59 -0800

    Setup monorepo

---
+++ scripts/unit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ${OKTA_HOME}/${REPO}/scripts/setup.sh
+
+export TEST_SUITE_TYPE="junit"
+export TEST_RESULT_FILE_DIR="${REPO}/test-reports/unit"
+
+echo ${TEST_SUITE_TYPE} > ${TEST_SUITE_TYPE_FILE}
+echo ${TEST_RESULT_FILE_DIR} > ${TEST_RESULT_FILE_DIR_FILE}
+
+if ! yarn test; then
+  echo "Unit tests failed! Exiting..."
+  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
+fi
+
+exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_SUCCEED_IF_NO_RESULTS}
