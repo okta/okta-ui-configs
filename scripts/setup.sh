0c1a95813dc6a54b51b890c573bbb37a7b839b0e
jmelberg <jordan.melberg@okta.com>
2021-04-02 22:22:10 +0000

    Change yarn.lock artifactory urls
    OKTA-382401
    <<<Jenkins Check-In of Tested SHA: 86ff0f34f54745fc64e4e0cfccf7097c20be0834 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-ui-configs

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,7 +17,29 @@ else
 fi
 
 cd ${OKTA_HOME}/${REPO}
+
+#!/bin/bash
+YARN_REGISTRY=https://registry.yarnpkg.com
+OKTA_REGISTRY=${ARTIFACTORY_URL}/api/npm/npm-okta-master
+
+# Yarn does not utilize the npmrc/yarnrc registry configuration
+# if a lockfile is present. This results in `yarn install` problems
+# for private registries. Until yarn@2.0.0 is released, this is our current
+# workaround.
+#
+# Related issues:
+#  - https://github.com/yarnpkg/yarn/issues/5892
+#  - https://github.com/yarnpkg/yarn/issues/3330
+
+# Replace yarn artifactory with Okta's
+echo "Replacing $YARN_REGISTRY with $OKTA_REGISTRY within yarn.lock files..."
+sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#g" yarn.lock
+
 if ! yarn install; then
   echo "yarn install command failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
+
+# Revert the original change
+echo "Replacing $OKTA_REGISTRY with $YARN_REGISTRY within yarn.lock files..."
+sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" yarn.lock
