b77084d26ed7996410679f193b1fd3e69d3a69e5
Ganesh Somasundaram <ganesh.somasundaram@okta.com>
2022-04-11 16:54:08 +0000

    upgrade libraries to fix security vulns bump version
    OKTA-473588
    <<<Jenkins Check-In of Tested SHA: 3852e5290c7093cebae374270ccebba31b850d12 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-ui-configs
    Files changed count: 10
    PR Link: "https://github.com/okta/okta-ui-configs/pull/13"

--- packages/eslint-plugin-okta/lib/rules/no-unlocalized-text.test.js
+++ packages/eslint-plugin-okta/lib/rules/no-unlocalized-text.test.js
@@ -2,7 +2,7 @@ const rule = require('./no-unlocalized-text');
 const RuleTester = require('eslint').RuleTester;
 
 const ruleTester = new RuleTester({
-  parser: require.resolve('babel-eslint'),
+  parser: require.resolve('@babel/eslint-parser'),
   parserOptions: {
     sourceType: 'module',
   },
